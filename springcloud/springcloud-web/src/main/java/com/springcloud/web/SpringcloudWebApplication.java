edc6f9598e0f8ed12e1896ed478dc5ccab4252a5
Darcy-Hu <75243810+Darcy-Hu@users.noreply.github.com>
2021-01-19 18:21:28 +0800

    20210119_update_springcloud

--- springcloud/springcloud-web/src/main/java/com/springcloud/web/SpringcloudWebApplication.java
+++ springcloud/springcloud-web/src/main/java/com/springcloud/web/SpringcloudWebApplication.java
@@ -1,10 +1,13 @@
 package com.springcloud.web;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
 public class SpringcloudWebApplication {
 
 	public static void main(String[] args) {
--- springcloud/springcloud-web/src/main/java/com/springcloud/web/controller/DemoController.java
+++ springcloud/springcloud-web/src/main/java/com/springcloud/web/controller/DemoController.java
@@ -1,4 +1,16 @@
 package com.springcloud.web.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/demo")
 public class DemoController {
+
+    @GetMapping("/helloWorld")
+    public String helloWorld(String str){
+        return str;
+    }
+
 }
